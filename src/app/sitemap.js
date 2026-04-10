b5f4dab6959b078c40d081c2cc8f3cf943abae00
Ritesh <191220051+Zayrix-bit@users.noreply.github.com>
2026-04-10 13:44:58 +0530

    feat: add sitemap.js to define site structure and metadata for SEO

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,49 +1,10 @@
 export default function sitemap() {
-  const baseUrl = "https://jeevankart.in";
-  const currentDate = new Date().toISOString();
-
   return [
     {
-      url: baseUrl,
-      lastModified: currentDate,
+      url: "https://jeevankart.in",
+      lastModified: new Date().toISOString(),
       changeFrequency: "weekly",
       priority: 1.0,
-    },
-    {
-      url: `${baseUrl}/#about`,
-      lastModified: currentDate,
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#skills`,
-      lastModified: currentDate,
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: `${baseUrl}/#projects`,
-      lastModified: currentDate,
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/#services`,
-      lastModified: currentDate,
-      changeFrequency: "monthly",
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/#faq`,
-      lastModified: currentDate,
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: `${baseUrl}/#contact`,
-      lastModified: currentDate,
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
   ];
 }
